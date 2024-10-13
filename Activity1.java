aceb7da7c61de1a391e0dbc124b5cb58d6e35af0
Shpara15 <puneet.parakh@gmail.com>
2024-10-13 10:16:17 +0100

    Data types

---
+++ Activity1.java
@@ -0,0 +1,24 @@
+public class Activity1 {
+    public static void main(String[] args) {
+        int b=5;
+        int a=1;
+        int add = a+b;
+        int diff = a-b;
+        int mult = a*b;
+        float div = a/b;
+        String magic = "***MAGIC***";
+        System.out.println("Addition");
+        System.out.println("Addition of a and b = " +add);
+        System.out.println("Subtraction");
+        System.out.println("Subraction of b from a = " +diff);
+        System.out.println("Division");
+        System.out.println("Division of b from a = " +div);
+        System.out.println("Multiplication");
+        System.out.println("Multiplication of a and b = " +mult);
+        System.out.println(magic);
+        System.out.println("Addition was 1+5. Subtraction was 1-5. Multiplication was 1 x 5. Division was 1/5");
+
+
+    }
+    
+}
