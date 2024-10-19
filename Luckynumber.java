44d9dd7ca5bff7169f0e8cee80c30d823563c02a
Shpara15 <puneet.parakh@gmail.com>
2024-10-19 10:14:15 +0100

    Luckynumber.java

---
+++ Luckynumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Luckynumber {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Hello user, please enter your name.");
+    String name = sc.nextLine();
+    System.out.println("Enter your lucky number.");
+    int lucky= sc.nextInt();
+    System.out.println("Hello"+name+", your lucky number is Shlok"+lucky);
+
+}
+    
+}
