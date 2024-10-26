d6e8d565de5e2b595e70dedfc52cb928ffe15189
Shpara15 <puneet.parakh@gmail.com>
2024-10-26 15:22:08 +0100

    Calculator

--- Parent.java
+++ Parent.java
@@ -1,24 +1,39 @@
 class Main1 {
+
     public static void main(String[] er) {
+
         Child x = new Child();
+
         x.Naming("Shlok");
+
         x.ageN(14);
-        String name;
-    }
-}public class Parent {
-    int age,id;
-    String name;
-    void Naming(String name){
-        System.out.println("Name: "+name);
+
     }
 
-    
 }
-class Child extends Parent{
-    void ageN(int age)
-    void name(String name)
-    {
-        System.out.println("The age of student "+name+" is "+age);
+
+class Parent {
+
+    int age, id;
+
+    String name;
+
+    void Naming(String name) {
+
+        this.name = name; // Store the name in the instance variable
+
+        System.out.println("Name: " + name);
 
     }
+
+}
+
+class Child extends Parent {
+
+    void ageN(int age) {
+
+        System.out.println("The age of student " + name + " is " + age);
+
+    }
+
 }
