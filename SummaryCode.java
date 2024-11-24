3a81451881c4af1f6f5d455025aeb8477e38eb1e
Shpara15 <puneet.parakh@gmail.com>
2024-11-24 12:11:19 +0000

    DoneACP

--- SummaryCode.java
+++ SummaryCode.java
@@ -50,8 +50,77 @@ public class SummaryCode {
                 int accountIndex;
                 String temp;
 
-                
+                accountIndex = sc.nextInt();
+                sc.nextLine();
 
+                if (accountIndex > size){
+                    System.out.println("Account does not exist.");
+                    System.out.println("Termininating...");
+                }
+                else{
+                    temp = accountName[accountIndex];
+                    System.out.println("Enter the new name: ");
+                    String name = sc.nextLine();
+                    accountName[accountIndex] = name;
+                    System.out.println("Name is updated from "+temp+" to "+name+".\n");
+                }
+                System.out.println("====================");
+
+            }
+            else if(option==3){
+                System.out.println("Check Account Balance Menu");
+                System.out.println("Enter your Account Number: ");
+
+                int accountIndex;
+                accountIndex = sc.nextInt();
+
+                if (accountIndex > size){
+                    System.out.println("Account does not exist.");
+                    System.out.println("Termininating...");
+
+                }
+                else{
+                    System.out.println(accountName[accountIndex]+" your balance is "+accountBalance[accountIndex]+"Rs.");
+                    
+                }
+                System.out.println("===================");
+            }
+            else if(option==4){
+                System.out.println("Update Balance Account Menu");
+                System.out.println("Enter your Account Number: ");
+
+                int accountIndex;
+                accountIndex = sc.nextInt();
+
+                if(accountIndex > size){
+                    System.out.println("Account does not exist.");
+                    System.out.println("Termininating...");
+                }
+                else{
+                    System.out.println("Enter the amount to be deposited: ");
+                    double amt = sc.nextDouble();
+                    accountBalance[accountIndex] += amt;
+                    System.out.println(accountName[accountIndex]+" your updated balance is: "+ accountBalance[accountIndex]+"Rs. \n");
+
+                }
+                System.out.println("====================");
+            }
+            else if(option==5){
+                System.out.println("Accounts registered\n");
+                for(int i=100;i<size;i++){
+                    System.out.println("Account Number: "+i+", Name: "+accountName[i]+", Balance: "+accountBalance[i]+"Rs\n");
+                }
+                System.out.println("====================");
+            }
+            else if(option==6){
+                System.out.println("Terminating...");
+                infiniteLoop = false;
+            }
+            else{
+                System.out.println("Invalid Input.");
+                System.out.println("Terminating...");
+
+                System.exit(0);
             }
 
         
