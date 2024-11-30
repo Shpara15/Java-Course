fc5a4764089c7dc1ab91e8ec8340738c57ff223d
Shpara15 <puneet.parakh@gmail.com>
2024-11-30 09:27:29 +0000

    UI.ACP

---
+++ UI.java
@@ -0,0 +1,13 @@
+public class UI {
+    public static void main(String[] args) {
+        System.out.println("The UI views are:");
+        System.out.println("\n-> Text View");
+        System.out.println("\n-> Edit Text View");
+        System.out.println("\n-> Check Box View");
+        System.out.println("\n-> Radio Button View");
+        System.out.println("\n-> Image View");
+        System.out.println("\n-> Image Button View");
+        System.out.println("\n-> Button View");
+    }
+    
+}
